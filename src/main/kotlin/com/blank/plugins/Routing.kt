0054cbda6196adf55bf9b43ba5dac6c28db09d6a
mark.shmarov <shmarov.mark@gmail.com>
2024-10-03 20:46:20 +0200

    Initial commit

---
+++ src/main/kotlin/com/blank/plugins/Routing.kt
@@ -0,0 +1,31 @@
+package com.blank.plugins
+
+import io.github.smiley4.ktorswaggerui.SwaggerUI
+import io.ktor.server.application.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    install(SwaggerUI) {
+        swagger {
+            swaggerUrl = "swagger-ui"
+            forwardRoot = true
+        }
+        info {
+            title = "Example API"
+            version = "latest"
+            description = "Example API for testing and demonstration purposes."
+        }
+        server {
+            url = "http://localhost:8080"
+            description = "Development Server"
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
---
+++ src/main/kotlin/com/blank/service/UrlShorteningService.kt
@@ -0,0 +1,5 @@
+package com.blank.service
+
+class UrlShorteningService {
+    
+}
